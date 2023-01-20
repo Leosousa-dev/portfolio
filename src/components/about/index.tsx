7210c4fb84a36c61eb70b2282a15585f95d10952
leonardo sousa <leonardocasa790@gmail.com>
2023-01-20 13:00:21 -0300

    UP-about

--- src/components/about/index.tsx
+++ src/components/about/index.tsx
@@ -8,7 +8,6 @@ export const About = ()=>{
            <Subtitle content="Sobre mim"/>
             <S.Resume>
                 Me chamo Leonardo, estudo programação a 2 anos com foco em desenvolvimento front-end.
-                Sou apaixonado por tecnologia
                 Busco sempre novos métodos e tecnologias para me tornar um profissional cada vez melhor.
                 Atualmente estou em busca da minha primeira oportunidade profissional.
             </S.Resume>
